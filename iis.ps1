6594e3d4f90998785460105ae866a6ea330e6e4d
Bruno Gabriel da Silva <38823062+bgsilvait@users.noreply.github.com>
2020-02-22 00:50:17 +0000

    Delete iis.ps1

--- iis.ps1
+++
@@ -1,3 +0,0 @@
-#Add X-Forward-For IIS Default website
-Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/siteDefaults/logFile/customFields" -name "." -value @{logFieldName='X-Forwarded-For'
-;sourceName='X-Forwarded-For';sourceType='RequestHeader'}
